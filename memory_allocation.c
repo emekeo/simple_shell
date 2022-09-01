cdcf91e73e738e9f0d7d8d9814e9dcd324003fbc
Mckennastan <oikenna3@gmail.com>
2022-09-01 05:18:53 -0700

    The realloc function program helps to distribute a memory block to variables

--- memory_allocation.c
+++ memory_allocation.c
@@ -1,11 +1,13 @@
 #include "shell.h"
 
 /**
+ * 
  * _realloc - function reallocates a pointer to double the space
  * @ptr: pointer to the old array
+ *
  * @size: pointer to number of elements in the old array
  *
- * Return: pointer to the new array
+ * Return: The pointer to the new array
  */
 char **_realloc(char **ptr, size_t *size)
 {
